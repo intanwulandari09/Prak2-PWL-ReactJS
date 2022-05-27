4be4f22145048205cfb28cdb4f5bc597194a8ad4
intanwulandari09 <91053438+intanwulandari09@users.noreply.github.com>
2022-05-27 23:57:53 +0700

    Add files via upload

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/foto intan.jpeg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2">
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hai semua! I am <strong>&nbsp;Intan Wulandari Siregar</strong>
+                <br />Aku adalah mahasiswa teknik informatika institut teknologi sumatera
+                angakatan 2019.
+                <br />
+                Aku sekarang semester 6, banyak beban dan juga tugas 
+                kadang aku juga bingung mau ngerjain yang mana , karena tugas
+                nya banyak
+                <br />
+                Tapi gapapa, aku bisa mengembalikan mood ku dengan 
+                tidur, sholat dan jalan-jalan sendiri 
+                <br />
+                Aku tau ini berat tapi aku juga pasti bisa melaluinya
+                <br />Thingking isn't doing :)
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Ayo Ngobrol
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/intanwulandari09"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
